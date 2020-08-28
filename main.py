c8f25086d32c1ba5c373bb4df9bac531f85bf8ed
qfc5036 <qfc5036@psu.edu>
2020-08-28 08:20:12 +0000

    k

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-temperture =input("Enter temperture in celsius: ")
-temperture =float(temperture)
-print(str(temperture) + chr(176) + "in Celsius is equivalent to" + str(temperture *1.8 + 32) + chr(176) + "Fahrenheit")
+temperature =input("Enter temperature in celsius: ")
+temperature =float(temperature)
+print(str(temperature) + chr(176) + "in Celsius is equivalent to" + str(temperature *1.8 + 32) + chr(176) + "Fahrenheit")
